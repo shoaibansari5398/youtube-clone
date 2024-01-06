ac1039131e597de55c0d9cfed28a29c7bef069be
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2024-01-06 16:30:10 +0530

    added search functionality with throttling and cache && comments

--- src/Components/Head.js
+++ src/Components/Head.js
@@ -1,17 +1,44 @@
-
-import React from "react";
-import { useDispatch } from "react-redux";
-import {toggleMenu} from "../utils/appSlice"
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/constants";
+import { cacheResults } from "../utils/searchSlice";
 
 const Head = () => {
+	const [searchQuery, setSearchQuery] = useState("");
+	const [suggestions, setSuggestions] = useState([]);
+	const [showSuggwstions, setShowSuggestions] = useState(false);
 
 	const dispatch = useDispatch();
 
+	const searchCache = useSelector(store=>store.search);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (searchCache[searchQuery]) {
+				setSuggestions(searchCache[searchQuery]);
+			} else {
+				getSearchSuggestions();
+			}
+		}, 200);
+
+		return () => clearTimeout(timer);
+	}, [searchQuery]);
+
+	const getSearchSuggestions = async () => {
+		const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+		const json = await data.json();
+		setSuggestions(json[1]);
+
+		dispatch(cacheResults({
+			[searchQuery]:json[1]
+		}))
+	};
 
 	const toggleMenuHandler = () => {
 		dispatch(toggleMenu());
-		console.log("toggleMenu")
-	}
+		console.log("toggleMenu");
+	};
 
 	return (
 		<div className="grid grid-flow-col p-5  shadow-lg">
@@ -28,10 +55,27 @@ const Head = () => {
 				/>
 			</div>
 			<div className="col-span-8">
-				<input className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full" />
-				<button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
-					Search
-				</button>
+				<div>
+					<input
+						onFocus={() => setShowSuggestions(true)}
+						onBlur={() => setShowSuggestions(false)}
+						value={searchQuery}
+						onChange={(e) => setSearchQuery(e.target.value)}
+						className="px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+					/>
+					<button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+						Search
+					</button>
+				</div>
+				{showSuggwstions && (
+					<div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+						<ul>
+							{suggestions.map((s) => (
+								<li className="py-2 px-3 shadow-sm hover:bg-gray-100">{s}</li>
+							))}
+						</ul>
+					</div>
+				)}
 			</div>
 			<div className="col-span-1">
 				<img
--- src/Components/WatchPage.js
+++ src/Components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
 	const [searchParams] = useSearchParams();
@@ -24,6 +25,7 @@ const WatchPage = () => {
 					allowFullScreen
 				></iframe>
 			</div>
+			<CommentsContainer/>
 		</div>
 	);
 };
